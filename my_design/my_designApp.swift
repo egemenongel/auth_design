3684a02975c3f21dce04b1925ec97fe1d5a09cbd
egemenongel <egemenongel@hotmail.com>
2024-01-25 17:29:46 +0300

    Initial Commit

---
+++ my_design/my_designApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_designApp.swift
+//  my_design
+//
+//  Created by Egemen Öngel on 25.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct my_designApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
