72f21d6606bfc96b413d6b25fec8e4289d87bb05
egemenongel <egemenongel@hotmail.com>
2024-01-28 16:53:00 +0300

    feat: Create Login View

--- my_design/LoginView.swift
+++ my_design/LoginView.swift
@@ -9,7 +9,44 @@ import SwiftUI
 
 struct LoginView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing:20){
+            Image("icon")
+                .renderingMode(.template)
+                .foregroundColor(Color.theme.background)
+            Text("Account Login")
+                .foregroundStyle(Color.theme.black)
+                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                .font(/*@START_MENU_TOKEN@*/.title2/*@END_MENU_TOKEN@*/)
+            Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ac ante ut tellus mollis tempor. Nullam et turpis vel dui accumsan dapibus a eget odio. Praesent id orci ut lectus ullamcorper ullamcorper. ")
+                .font(.body)
+                .multilineTextAlignment(.center)
+                .foregroundStyle(Color.theme.gray)
+            CustomButton(
+                text:"Register",
+                icon: Image(systemName: "arrow.right"),
+                clicked:{}
+            )
+            CustomButton(
+                text:"Start Now",
+                clicked:{},
+                foregroundColor:Color.theme.background,
+                backgroundColor:.white
+            )
+            Button(action: {}){
+                Text("How to register an application?")
+                    .underline(true)
+            }
+            .foregroundStyle(Color.theme.background)
+            .font(.body)
+            
+           
+            
+            
+            
+            
+        }
+        .padding(60.0)
+        
     }
 }
 
